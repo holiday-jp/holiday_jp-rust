081196096a0dd2673d9a65deca573a1640895d75
camelmasa <camelmasa@gmail.com>
2019-06-04 21:21:34 +0900

    chore: move unit tests

--- src/holiday_jp/holiday_jp.rs
+++ src/holiday_jp/holiday_jp.rs
@@ -22,3 +22,21 @@ impl HolidayJp {
         HOLIDAYS.is_holiday(date)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::HolidayJp;
+    use chrono::{Local, TimeZone};
+
+    #[test]
+    fn between() {
+        let holidays = HolidayJp::between(Local.ymd(2010, 9, 14), Local.ymd(2010, 9, 21));
+
+        assert_eq!(holidays.first().unwrap().name, "敬老の日");
+    }
+
+    #[test]
+    fn is_holiday() {
+        assert!(HolidayJp::is_holiday(Local.ymd(2016, 8, 11)));
+    }
+}
